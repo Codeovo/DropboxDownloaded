191d70aab34e55d5f48c1ba4a1377f6ad7b609ab
Mohammed Al-Dahleh <maldahleh@codeovo.io>
2017-03-22 05:12:45 -0400

    Minor tweak to command class

--- src/io/codeovo/dropboxdl/commands/UploadCommand.java
+++ src/io/codeovo/dropboxdl/commands/UploadCommand.java
@@ -135,6 +135,7 @@ public class UploadCommand implements CommandExecutor {
         } else {
             commandSender.sendMessage(prefix + "You do not have permission to use that command.");
         }
-        return false;
+
+        return true;
     }
 }
