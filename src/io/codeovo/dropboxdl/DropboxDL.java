e7633e4feb7266ad679a8867c64f20f75852d5df
Mohammed Al-Dahleh <maldahleh@codeovo.io>
2017-03-22 05:04:22 -0400

    Implementation of configuration file

--- src/io/codeovo/dropboxdl/DropboxDL.java
+++ src/io/codeovo/dropboxdl/DropboxDL.java
@@ -1,19 +1,27 @@
 package io.codeovo.dropboxdl;
 
+import io.codeovo.dropboxdl.commands.UploadCommand;
+
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DropboxDL extends JavaPlugin {
     private static DropboxDL dropboxDL;
+    private DbxClientV2 dbxClient;
 
     private String pluginConsolePrefix = "Dropbox DL >> ";
-    private DbxClientV2 dbxClient;
+    private String accessToken = "";
 
     @Override
     public void onEnable() {
         getLogger().info(pluginConsolePrefix + "Enabling...");
         dropboxDL = this;
+
+        loadConfiguration();
+        loadDropboxData();
+        registerCommands();
         getLogger().info(pluginConsolePrefix + "Enabled.");
     }
 
@@ -24,10 +32,22 @@ public class DropboxDL extends JavaPlugin {
         getLogger().info(pluginConsolePrefix + "Disabled.");
     }
 
-    private void loadDropboxData(String accessToken) {
+    private void loadConfiguration() {
+        getLogger().info(pluginConsolePrefix + "Loading config...");
+        saveDefaultConfig();
+
+        accessToken = getConfig().getString("access-token");
+    }
+
+    private void loadDropboxData() {
         getLogger().info(pluginConsolePrefix + "Connecting to Dropbox...");
         DbxRequestConfig config = new DbxRequestConfig("codeovo/dropbox-dl");
         dbxClient = new DbxClientV2(config, accessToken);
+    }
+
+    private void registerCommands() {
+        getLogger().info(pluginConsolePrefix + "Registering commands...");
+        getCommand("upload").setExecutor(new UploadCommand(this));
     }
 
     public static DropboxDL getInstance() { return dropboxDL; }
